e57366844b36a16b43aa7a5956d26e0720056b22
Aaron Yli-Hallila <>
2021-01-09 15:32:36 +0200

    Sort projects lexicographically

--- qupath-extension-edu/src/main/java/qupath/edu/models/ExternalSubject.java
+++ qupath-extension-edu/src/main/java/qupath/edu/models/ExternalSubject.java
@@ -1,9 +1,11 @@
 package qupath.edu.models;
 
+import qupath.edu.lib.NaturalOrderComparator;
 import qupath.edu.lib.RemoteOpenslide;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ExternalSubject {
@@ -40,12 +42,13 @@ public class ExternalSubject {
 
     /**
      * Returns an alphabetically sorted list of projects with hidden projects filtered out
+     *
      * @return List of ExternalProjects
      */
     public List<ExternalProject> getProjects() {
         return projects.stream()
                 .filter(project -> !(project.isHidden() && !RemoteOpenslide.isOwner(project.getOwner())))
-                .sorted(Comparator.comparing(ExternalProject::getName))
+                .sorted(Comparator.comparing(ExternalProject::getName, new NaturalOrderComparator<>()))
                 .collect(Collectors.toList());
     }
 
