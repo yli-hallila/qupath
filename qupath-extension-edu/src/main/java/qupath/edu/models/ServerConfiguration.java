826bbae1a982c726244b1d11e8d1eb6610866c29
Aaron Yli-Hallila <>
2021-01-23 16:37:34 +0200

    Use Version class in ServerConfiguration

--- qupath-extension-edu/src/main/java/qupath/edu/models/ServerConfiguration.java
+++ qupath-extension-edu/src/main/java/qupath/edu/models/ServerConfiguration.java
@@ -1,20 +1,22 @@
 package qupath.edu.models;
 
+import qupath.lib.gui.Version;
+
 public class ServerConfiguration {
 
-    private final String version;
+    private final Version version;
     private final boolean guestLoginEnabled;
     private final boolean simpleLoginEnabled;
     private final boolean microsoftLoginEnabled;
 
-    public ServerConfiguration(String version, boolean guestLoginEnabled, boolean simpleLoginEnabled, boolean microsoftLoginEnabled) {
+    public ServerConfiguration(Version version, boolean guestLoginEnabled, boolean simpleLoginEnabled, boolean microsoftLoginEnabled) {
         this.version = version;
         this.guestLoginEnabled = guestLoginEnabled;
         this.simpleLoginEnabled = simpleLoginEnabled;
         this.microsoftLoginEnabled = microsoftLoginEnabled;
     }
 
-    public String getVersion() {
+    public Version getVersion() {
         return version;
     }
 
